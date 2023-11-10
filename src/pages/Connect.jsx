3266c65a7ccdda67535c19b6d5cee3e90f673e14
Klhee Jacobs <bangklhee@gmail.com>
2023-11-09 23:34:36 -0600

    final

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -6,8 +6,8 @@ function Connect() {
         <div className='connect-text'>I would love to connect! You can find me at:</div>
         <div className='connect-social'>
           <ul><a id='connect-links' href = "mailto: klhee.jacobs@gmail.com"><i class="fa-solid fa-envelope"></i></a></ul>
-          <ul><a id='connect-links' href="https://www.linkedin.com/in/klhee-jacobs-99743a161/" target='_blank'><i class="fa-brands fa-linkedin"></i></a></ul>
-          <ul><a id='connect-links' href="https://www.instagram.com/klheee/" target='_blank'><i class="fa-brands fa-instagram"></i></a></ul>
+          <ul><a id='connect-links' href="https://www.linkedin.com/in/klhee-jacobs-99743a161/"><i class="fa-brands fa-linkedin"></i></a></ul>
+          <ul><a id='connect-links' href="https://www.instagram.com/klheee/"><i class="fa-brands fa-instagram"></i></a></ul>
         </div>
       </div>
     </div>
